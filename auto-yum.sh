535db81d149e5a3332d6494a80fcd281c307e6dc
ajibjawwad <ajib.jawwad@gmail.com>
2017-12-18 15:27:55 +0700

    Create auto-yum.sh

---
+++ auto-yum.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# make by me: ajib
+# simple auto yum update running on cenots 7 and making log about its update if update success or fail
+
+TIME=$(date +%b-%d-%Y-%H:%M:%S)
+NOW=$(date +%Y%m%d)
+CMDYUM='yum update -y'
+
+echo "$TIME Update running!" >> /var/log/autoyum-$NOW.log
+
+$CMDYUM >> /var/log/autoyum-$NOW.log 2>&1
+#$EXPORTLOG 2>&1
+if echo $? -eq 0; then
+        echo "$TIME Update successfull !" >> /var/log/autoyum-$NOW.log
+        exit 0
+else
+        echo "$TIME Update error!" >> /var/log/autoyum-$NOW.log
+        exit 1
+fi
