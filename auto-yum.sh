c853ea00c6b48e1a0c078d088ce537a14ea4d44e
ajibjawwad <ajib.jawwad@gmail.com>
2017-12-18 15:48:28 +0700

    Update auto-yum.sh

--- auto-yum.sh
+++ auto-yum.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# make by me: ajib -> ajib.jawwad@gmail.com
-# simple auto yum update running on cenots 7 and making log about its update if update success or fail
+# created by    : ajib
+# email         : ajib.jawwad@gmail.com
+# date          : 18-12-2017
+# desc          : simple auto yum update running on cenots 7 and making log about its update if update success or fail
 
 TIME=$(date +%b-%d-%Y-%H:%M:%S)
 NOW=$(date +%Y%m%d)
